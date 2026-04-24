ddf8d925195f34c002492c06106c9ac3ce828dbd
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-24 19:08:31 +0600

    add 3 loop

--- loopBasic.js
+++ loopBasic.js
@@ -1,3 +1,28 @@
 for(let i = 1; i <=3; i++){
-    console.log("hello");
+    // console.log("hello");
 }
+
+for(let i = 1;i < 5; i++){
+    // console.log(i);
+}
+
+// while loop 
+
+let i = 1;
+while(i <= 7){
+    // console.log(i);
+    i++
+}
+
+let e = 2;
+while(e <= 10){
+    // console.log(e);
+    e++ 
+}
+
+// do... while loop 
+let u = 1;
+do{
+    console.log(u);
+    u++
+}while(u <= 6);
