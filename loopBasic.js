ed838c8e80ba23037c959e2c361ebf6c2b0eba55
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-24 20:38:31 +0600

    score update

--- loopBasic.js
+++ loopBasic.js
@@ -1,54 +1,54 @@
-for(let i = 1; i <=3; i++){
-    // console.log("hello");
-}
+// for(let i = 1; i <=3; i++){
+//     // console.log("hello");
+// }
 
-for(let i = 1;i < 5; i++){
-    // console.log(i);
-}
+// for(let i = 1;i < 5; i++){
+//     // console.log(i);
+// }
 
-// while loop 
+// // while loop 
 
-let i = 1;
-while(i <= 7){
-    // console.log(i);
-    i++
-}
+// let i = 1;
+// while(i <= 7){
+//     // console.log(i);
+//     i++
+// }
 
-let e = 2;
-while(e <= 10){
-    // console.log(e);
-    e++ 
-}
+// let e = 2;
+// while(e <= 10){
+//     // console.log(e);
+//     e++ 
+// }
 
-// do... while loop 
-let u = 1;
-do{
-    // console.log(u);
-    u++
-}while(u <= 6);
+// // do... while loop 
+// let u = 1;
+// do{
+//     // console.log(u);
+//     u++
+// }while(u <= 6);
 
-let fruits = ["apple","banana","mango"];
+// let fruits = ["apple","banana","mango"];
 
-for(let fruit of fruits){
-    // console.log(fruit);
-}
+// for(let fruit of fruits){
+//     // console.log(fruit);
+// }
 
-let items = document.querySelectorAll("li")
+// let items = document.querySelectorAll("li")
 
-for(let item of items){
-    item.style.color = "red"
-    item.style.background = "green"
-    item.style.padding = "10px"
-}
+// for(let item of items){
+//     item.style.color = "red"
+//     item.style.background = "green"
+//     item.style.padding = "10px"
+// }
 
-let buttons = document.querySelectorAll(".btn")
+// let buttons = document.querySelectorAll(".btn")
 
-for(let button of buttons){
-    button.addEventListener("click", function(){
-        alert("button click");
+// for(let button of buttons){
+//     button.addEventListener("click", function(){
+//         alert("button click");
         
-    })
-}
+//     })
+// }
 
 // =====================
 let list = document.getElementById("list")
@@ -57,4 +57,50 @@ for(let i = 1;i <= 6;i++){
     let li = document.createElement("li")
     li.innerText = "LIST" + i
     list.appendChild(li)
+}
+
+// =============================
+
+// let buttons = document.querySelectorAll(".scoreRun");
+// let total = 0;
+// let totalDisplay = document.getElementById("total");
+
+// for(let btn of buttons){
+//   btn.addEventListener("click", (e)=>{
+//     let value = Number(e.target.innerText);
+//     total += value;
+//     totalDisplay.innerText = total;
+//   });
+// }
+
+
+// console.log("JS loaded");
+
+//     let buttons = document.querySelectorAll(".scoreRun");
+//     console.log(buttons); // check buttons
+
+//     let total = 0;
+//     let totalDisplay = document.getElementById("total");
+
+//     for(let btn of buttons){
+//       btn.addEventListener("click", (e)=>{
+//         console.log("clicked"); // check click
+
+//         let value = Number(e.target.innerText);
+//         console.log("value:", value);
+
+//         total += value;
+//         totalDisplay.innerText = total;
+//       });
+//     }
+
+let buttons = document.querySelectorAll(".scoreRun");
+let total = 0;
+
+for(let btn of buttons){
+  btn.addEventListener("click", function(){
+    let value = parseInt(btn.innerText);
+    total += value;
+    document.getElementById("total").innerText = total;
+  });
 }
